2e98e1b56e57f9835c434009e81f3d5ea8ed4151
kes0042 <kes0042@mix.wvu.edu>
2021-02-12 10:13:23 -0500

    Katelyn Skaggs Task 04

--- Task_03/task03.r
+++ Task_03/task03.r
@@ -81,11 +81,40 @@ Pop <- simPop(50)
 points(Pop[,"freqa"], Pop[, "Genotypes.aa"]/ 50, pch=22, bg="red")
 # the amount in the lower frequencies aapear
 
-library('learnPopGen')
+install.packages("learnPopGen")
+library(learnPopGen)
+x <- genetic.drift(Ne=200, nrep=5, pause=0.01)  # the graph data is recorded between .8 to .1 and includes multiple increases and decreases 
+x <- genetic.drift(Ne=400, nrep=5, pause=0.01)  # the graph shrunk to the data falling between .2 and .8 but there are still many increases and decreases
+x <- genetic.drift(Ne=700, nrep=5, pause=0.01)  # this shrunk the data to between .3 and .8 there are also not as drastic increases and decreases
+PopSizes <- 5:50  #makes a bunch of populations of different sizes between 5 and 50 individuals 
+Samples <- 5:50 #say that there are 5 populations with each given size		
+#simulate all 230 of populations and get the time one of the two allels went extincit 
+tExt <- sapply(Samples, function(x) nrow(simPop(x, 500)))
+Line <- lm(tExt ~ Samples)  # lm() used to fit a line to data 
+summary(Line)
+Line$coef # extract coefficients use $coef
+plot(Samples, tExt)
+abline(Line)
+# graph has a gradual increasing line with data plots throughout the graph the highest at 45 samples at tExt 400 
+
+#compare
+Line2 <-lm(tExt~Samples+0)
+summary(Line2)
+Line2$coef
+plot(Samples, tExt)
+abline(Line2)
+# there is not nearly as a steep increase as in the other graph the hightest is still at the same as the last the data is much more closer to the line overall than that first graph
+# the data spots are father from the line overall in the first graph and closer to the line in the second graph
 
 
-
-
+#extra credit
+head(Line)
+summary(Line)
+Line.ols <- Line <- lm(tExt ~ Samples)
+install.packages("ggplot2")
+library("ggplot2")
+ggplot(date= Line, aes(y= Samples, x= tExt)) + geom_point(col= "blue") + geom_abline(slope = 0)
+rlang::last_error()
 
 
 
