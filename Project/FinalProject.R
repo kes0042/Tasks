3b6a5a9d1aac8459685d7270a9501d6198ed8688
kes0042 <kes0042@mix.wvu.edu>
2021-04-02 11:00:11 -0400

    Katelyn Skaggs Task 08 upload a

--- Project/FinalProject.R
+++ Project/FinalProject.R
@@ -1,12 +1,48 @@
 # Hypothesis:
-# There is a positive correlation between the total begging call time measured in seconds between Vidua macroua and their host species Extrilda astrid. #
+# There is a positive correlation between the mass and the basal metabolic rate in a wide range of mammals. #
 
 # Import data ####
 #rm (list = ls())
+library(phytools)
+library(nlme)
+library(RRphylo)
+
 setwd("~/Desktop/Evolution/Tasks/Project")
-mydata <- read.csv ("Evolutionproject.csv")
-mydata
+# read in data
+mydata <- read.csv ("MassBMRproject.csv")
+head(mydata)
 
+# rename data to match phylogeny
+mydata[,1] <- gsub(" ", "_", mydata[,1])
 
-par(mar=c(4,5,1,1), las=1, tck=-0.01, mgp=c(3,0.5,0))
-boxplot(mydata[,4]~mydata[,1], xlab="species", ylab="call time", boxwex=0.2, col=c("#b3e2cd", "#fdcdac"))
+# read in trees
+Trees <- read.nexus("output.nex")
+tree <- Trees[[1]]
+
+# find species missing from phylogeny
+Drops <- setdiff(mydata[,1], tree$tip.label)
+DropRows <- sapply(Drops, function(x) grep(x, mydata[,1]))
+mydata2 <- mydata[-DropRows,]
+colnames(mydata2) <- c("species", "raw_bm", "logbm", "raw_bmr", "logbmr")
+
+Cor <- corBrownian(1, tree)
+
+par(mar=c(4,5,1,1), mgp=c(2.5,0.3,0), tck=-0.01, las=1)
+plot(mydata2[,5], mydata2[,3], pch=16, col=rgb(0,0,0,0.5), xlab="log10 BMR", ylab="log10 mass")
+
+# incorrect regression line--No phylogeny
+NoPhy <- lm(mydata2[,3]~mydata2[,5])
+abline(NoPhy, col='red', lty=2)
+
+# phylogenetically corrected linear regression
+model01 <- gls(logbm~logbmr, data=mydata2, correlation=Cor)
+abline(model01, col='blue', lty=2, lwd=2)
+
+#Red= is the just data not corrected relationship
+
+#blue= is the phylegentic corrected one 
+
+logBMR <- setNames(mydata2[,"logbmr"], mydata2[,"species"])
+RRbmr <- RRphylo(tree, logBMR)
+logBM <- setNames(mydata2[,"logbm"], mydata2[,"species"])
+RRbm <- RRphylo(tree, mydata2[,"logbm"], cov=RRbmr, x1=mydata2[,"logbmr"])
---
+++ Task_08/task08.r
@@ -0,0 +1,50 @@
+setwd("~/Desktop/Evolution/Tasks/Task_08")
+library("phytools")
+tree <-read.tree("https://jonsmitchell.com/data/anolis.tre")
+plot(tree, type="fan")
+#question 1: 82 tips
+
+data <- read.csv("https://jonsmitchell.com/data/svl.csv", stringsAsFactors=F, row.names=1)
+dim(data)
+class(data)
+
+#question2: there are 82 dimensions and 1 branch
+
+svl <- setNames(data$svl, rownames(data))
+svl
+
+Ancestors <- fastAnc(tree, svl, vars=TRUE, CI=TRUE)
+#question 3: stored in the tips which is svl .     The CI95 elements are on the ancestral state.
+
+#quesiton 4: the function computes the contrast state at the root and re-roots the tree at interval nodes
+
+
+par(mar=c(0.1,0.1,0.1,0.1))
+plot(tree, type="fan", lwd=2, show.tip.label=F)
+
+# labels the tips with points instead of words using pch 
+tiplabels(pch=16, cex=0.25*svl[tree$tip.label])
+# adds the ancestral sates 
+nodelabels(pch=16, cex=0.25*Ancestors$ace)
+# visiualize the data in different way to see the differences in the tips 
+obj <- contMap(tree, svl, plot=F)
+plot (obj, type="fan", legend= 0.7*max(nodeHeights(tree)), sig=2, fsize=c(0.7, 0.9))
+fossilData <- data.frame(svl=log(c(25.4, 23.2, 17.7, 19.7, 24, 31)), tip1=c("Anolis_aliniger", "Anolis_aliniger", "Anolis_occultus", "Anolis_ricordii", "Anolis_cristatellus", "Anolis_occultus"), tip2=c("Anolis_chlorocyanus", "Anolis_coelestinus", "Anolis_hendersoni", "Anolis_cybotes", "Anolis_angusticeps", "Anolis_angusticeps"))
+# question 5: 
+fossilNodes <-c()
+nodeN <-c()
+
+plot(tree, cex=0.5)
+# question 5:
+for (i in 1:nrow(fossilData))	{
+	Node <- fastMRCA(tree, fossilData [i,"tip1"], fossilData[i,"tip2"])
+	fossilNodes[i] <- fossilData[i, "svl"]
+	nodeN[i] <- Node
+	nodelabels(node=Node, pch=21, bg="red", cex=1.2)
+}
+names(fossilNodes) <- nodeN
+Ancestors_withFossils <- fastAnc(tree, svl, anc.states=fossilNodes, CI=TRUE, var=TRUE)
+
+# question 7: fossils change the estimated ancestral sizes because you can look at the fossils and determine based off of how far the species is on the tree the relative size you can also look at the speices between two fossils and do the same thing. 
+# question 8-10:
+install.packages("geiger")
