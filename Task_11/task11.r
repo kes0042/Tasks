f39a89bcd58b792bf7aeb32ae0cd82ec68c8f5f0
kes0042 <kes0042@mix.wvu.edu>
2021-04-29 12:15:51 -0400

    Katelyn Skaggs Task 11 upload a

--- Task_11/task11.r
+++ Task_11/task11.r
@@ -1,2 +1,22 @@
 setwd("~/Desktop/Evolution/Tasks/Task_11")
 x <- rnorm(100, mean=5, sd =2)
+head(x)
+y <- (x*5) +2 +rnorm(100, 0:0.1)
+y
+x
+plot(x,y)
+abline(x,y, col="blue")
+
+# the slope is around -0.3185 and the y intercept ot arounf .1967.  This is because of the random numbers, and adding more will make these conclusions to change a little bit. 
+z <- c()
+x <- rnorm(100, mean=5, sd=2)
+for (i in 1:100) 
+	 z[i] <- rnorm(100, mean=5, sd=2) 
+	 y <- (x * z[i]) + 2 + (rnorm(100, 0:0.1)) 
+	 x <- (z[1:100]) 
+
+plot(z[1:100])
+abline(lm(y~z[1:100]))
+plot(c(z, -.32))
+
+install.packages("meme")
